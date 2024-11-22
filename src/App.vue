2fd3dce279dbf84a67e57b5986a4a4ec59f05598
Eyüp <epsilonelittr@gmail.com>
2024-11-22 22:23:23 +0300

    Add tailwind config editor

--- src/App.vue
+++ src/App.vue
@@ -52,22 +52,34 @@
               : '500px',
         }"
       >
-        <Tabs :tabs="tabs" defaultTab="tab1">
+        <Tabs :tabs="tabs" defaultTab="tab2">
           <template #tab1>
             <MonacoEditor
-              ref="monacoEditorRef"
-              :code="codeBlock"
+              :code="tailwindConfig"
               :height="
                 selectedLayout.key === 'horizontal'
                   ? 'calc(100vh - 60px)'
                   : '440px'
               "
+              language="javascript"
               :layout="selectedLayout"
-              @change="updateCodeBlock($event)"
+              @change="updateConfigBlock($event)"
             />
           </template>
           <template #tab2>
-            <div>This is content for Tab 2.</div>
+            <div>
+              <MonacoEditor
+                ref="monacoEditorRef"
+                :code="codeBlock"
+                :height="
+                  selectedLayout.key === 'horizontal'
+                    ? 'calc(100vh - 60px)'
+                    : '440px'
+                "
+                :layout="selectedLayout"
+                @change="updateCodeBlock($event)"
+              />
+            </div>
           </template>
         </Tabs>
       </div>
@@ -146,15 +158,51 @@
 
   const monacoEditorRef = ref<any>(null);
 
+  const tailwindConfig = ref(`tailwind.config = {
+    theme: {
+    extend: {
+      colors: {
+        theme1: {
+          50: "#f1f6fd",
+          100: "#dfebfa",
+          200: "#c7dbf6",
+          300: "#a0c5f0",
+          400: "#87b2ea",
+          500: "#5386de",
+          600: "#3e6ad2",
+          700: "#3557c0",
+          800: "#30499d",
+          900: "#2c407c",
+          950: "#1f284c",
+        },
+        theme2: {
+          50: "#fef9ee",
+          100: "#fcf2d8",
+          200: "#f8e2b0",
+          300: "#f4cf88",
+          400: "#edab4a",
+          500: "#e99326",
+          600: "#da791c",
+          700: "#b55e19",
+          800: "#904a1c",
+          900: "#743e1a",
+          950: "#3f1e0b",
+        },
+      },
+    },
+  },
+}`);
+
   const previewBaseCodeTemplate = ref(`<!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"><\/script>
+    <script>#CONFIG#<\/script>
   </head>
   <body>
-    #REPLACE#
+    #HTML#
   </body>
   </html>`);
 
@@ -163,17 +211,76 @@
     { name: 'tab2', label: 'Editor' },
   ];
 
-  const codeBlock = ref(`<h1>hello world</h1>`);
+  const codeBlock = ref(`<!-- component -->
+<div class="w-full">
+    <nav class="bg-white shadow-lg">
+        <div class="md:flex items-center justify-between py-2 px-8 md:px-12">
+            <div class="flex justify-between items-center">
+               <div class="text-2xl font-bold text-gray-800 md:text-3xl">
+                    <a href="#">Brand</a>
+               </div>
+                <div class="md:hidden">
+                    <button type="button" class="block text-gray-800 hover:text-gray-700 focus:text-gray-700 focus:outline-none">
+                        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                            <path class="hidden" d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"/>
+                            <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="flex flex-col md:flex-row hidden md:block -mx-2">
+                <a href="#" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">Home</a>
+                <a href="#" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">About</a>
+                <a href="#" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">Contact</a>
+            </div>
+        </div>
+    </nav>
+    <div class="flex bg-white" style="height:600px;">
+        <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
+            <div>
+                <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                <p class="mt-2 text-sm text-gray-500 md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates. Cumque debitis dignissimos id quam vel!</p>
+                <div class="flex justify-center lg:justify-start mt-6">
+                    <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">Get Started</a>
+                    <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="#">Learn More</a>
+                </div>
+            </div>
+        </div>
+        <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
+            <div class="h-full object-cover" style="background-image: url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80)">
+                <div class="h-full bg-black opacity-25"></div>
+            </div>
+        </div>
+    </div>
+</div>`);
 
   const previewBaseCode = ref(
-    previewBaseCodeTemplate.value.replace('#REPLACE#', codeBlock.value)
+    previewBaseCodeTemplate.value
+      .replace('#HTML#', codeBlock.value)
+      .replace('#CONFIG#', tailwindConfig.value)
   );
 
   const updateCodeBlock = (code: string) => {
     codeBlock.value = code;
+
+    updatePreviewCode();
+  };
+
+  const updateConfigBlock = (code: string) => {
+    tailwindConfig.value = code;
+
+    updatePreviewCode();
+  };
+
+  const updatePreviewCode = () => {
     previewBaseCode.value = previewBaseCodeTemplate.value.replace(
       '#REPLACE#',
-      code
+      codeBlock.value
+    );
+
+    previewBaseCode.value = tailwindConfig.value.replace(
+      '#CONFIG#',
+      tailwindConfig.value
     );
   };
 
