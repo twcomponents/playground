cd00d3bbf94596908e1b02430af2dce388cb1ab6
Eyüp <epsilonelittr@gmail.com>
2024-11-22 13:16:28 +0300

    Add breakpoint buttons

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,73 @@
 <template>
-  <div class="flex flex-row">
-    <!-- Left -->
-    <div class="flex flex-col w-1/2">
-      <MonacoEditor
-        :code="codeBlock"
-        :height="'100vh'"
-        @change="updateCodeBlock($event)"
-      />
+  <div class="flex flex-col">
+    <!-- Header -->
+    <div
+      class="flex flex-row h-[60px] items-center px-4 border-b border-gray-300"
+    >
+      <h1 class="text-2xl font-bold">TailwindCSS Playground</h1>
     </div>
 
-    <!-- Right -->
-    <div class="flex flex-col w-1/2">
-      <iframe class="w-full h-full" :srcdoc="previewBaseCode"></iframe>
+    <!-- Body -->
+    <div class="flex flex-row">
+      <!-- Left -->
+      <div class="flex flex-col w-1/4">
+        <MonacoEditor
+          :code="codeBlock"
+          :height="'100vh'"
+          @change="updateCodeBlock($event)"
+        />
+      </div>
+
+      <!-- Right -->
+      <div class="flex flex-col w-3/4">
+        <!-- Header -->
+        <div class="flex flex-row border-b border-gray-300 p-2">
+          <!-- Device Breakpoints -->
+          <div class="flex flex-row gap-2">
+            <button
+              v-for="screenBreakPoint in screenBreakPoints"
+              :key="screenBreakPoint.label"
+              class="flex items-center justify-center w-10 h-10 border border-theme1-300 text-theme1-500 hover:text-theme1-600 rounded-md"
+              :class="{
+                'bg-theme2-200': screenBreakPoint === selectedBreakpoint,
+              }"
+              @click="selectedBreakpoint = screenBreakPoint"
+            >
+              <component
+                :is="screenBreakPoint.icon"
+                :style="{
+                  'stroke-width': '1px',
+                }"
+                class="size-5"
+              />
+            </button>
+          </div>
+
+          <div class="flex flex-row"></div>
+        </div>
+
+        <!-- Body -->
+        <iframe class="w-full h-full" :srcdoc="previewBaseCode"></iframe>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  // native
   import { ref } from 'vue';
 
+  // components
   import MonacoEditor from '@/shared/components/MonacoEditor.vue';
+
+  // icons
+  import {
+    Smartphone,
+    TabletSmartphone,
+    Tablet,
+    Laptop,
+    ScreenShare,
+  } from 'lucide-vue-next';
 
   const previewBaseCodeTemplate = ref(`<!DOCTYPE html>
 <html lang="en">
@@ -46,4 +94,39 @@
       code
     );
   };
+
+  const screenBreakPoints = [
+    {
+      key: 'sm',
+      icon: Smartphone,
+      width: 640,
+      label: 'Phone',
+    },
+    {
+      key: 'md',
+      icon: TabletSmartphone,
+      width: 768,
+      label: 'Phablet',
+    },
+    {
+      key: 'lg',
+      icon: Tablet,
+      width: 1024,
+      label: 'Tablet',
+    },
+    {
+      key: 'xl',
+      icon: Laptop,
+      width: 1280,
+      label: 'Laptop',
+    },
+    {
+      key: '2xl',
+      icon: ScreenShare,
+      width: 1536,
+      label: 'Desktop',
+    },
+  ];
+
+  const selectedBreakpoint = ref(screenBreakPoints[3]);
 </script>
